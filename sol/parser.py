6373b672e4980c1f6ef4e35005e30226b4595d4f
Ian Preston <hello@ianpreston.io>
2015-08-30 16:51:00 -0700

    Separate parser implementation from individual token specifications

--- sol/parser.py
+++ sol/parser.py
@@ -9,6 +9,16 @@ from sol.ast import (
 
 
 class Parser:
+    lbp_map = {}
+    nud_map = {}
+    led_map = {}
+
+    @classmethod
+    def register_token(cls, token_type, lbp, nud, led):
+        cls.lbp_map[token_type] = lbp
+        cls.nud_map[token_type] = nud
+        cls.led_map[token_type] = led
+
     def __init__(self, tokens):
         self.tokens = tokens
         self.idx = 0
@@ -29,67 +39,6 @@ class Parser:
     def program(self):
         return self.expr()
 
-    def nud(self, token):
-        if token.type == TokenType.IDENT:
-            return IdentAstNode(token.value)
-
-        if token.type == TokenType.STRING:
-            return ConstAstNode(ConstType.STRING, token.value)
-
-        if token.type == TokenType.INT:
-            return ConstAstNode(ConstType.INT, int(token.value))
-
-        if token.type == TokenType.SEMICOLON:
-            return 'EOF'
-
-        raise Exception('Invalid token for nud()', token)
-
-    def led(self, left, token):
-        # TODO - Registrar/map
-
-        if token.type in (
-            TokenType.IDENT,
-            TokenType.STRING,
-            TokenType.INT,
-            TokenType.SEMICOLON,
-        ):
-            return left
-
-        if token.type == TokenType.PASS:
-            expr = self.expr(self.lbp(token))
-            return MsgAstNode(
-                target=left,
-                name=expr,
-                args=None,
-            )
-
-        if token.type == TokenType.ASSIGNMENT:
-            expr = self.expr(self.lbp(token))
-            return AssignAstNode(left, expr)
-
-        if token.type == TokenType.LPAREN:
-            return self.led_arg_list(left, token)
-
-        if token.type == TokenType.RPAREN:
-            return left
-
-        raise Exception('Invalid token for led()', token)
-
-    def lbp(self, token):
-        # TODO - Registrar/map
-
-        if token.type == TokenType.PASS:
-            return 10
-        if token.type == TokenType.ASSIGNMENT:
-            return 9
-        if token.type == TokenType.IDENT:
-            return 8
-        if token.type in (TokenType.STRING, TokenType.INT):
-            return 7
-        if token.type == TokenType.SEMICOLON:
-            return 0
-        return 1
-
     def expr(self, rbp=0):
         left = self.nud(self.token)
         while rbp < self.lbp(self.token):
@@ -99,21 +48,100 @@ class Parser:
             left = self.led(left, self.last_token)
         return left
 
-    def led_arg_list(self, left, token):
-        args = []
+    def nud(self, token):
+        cable = self.nud_map.get(token.type)
+        if not cable:
+            raise Exception('Unexpected token', token)
+        return cable(token)
 
-        while True:
-            expr = self.expr(self.lbp(token))
-            args.append(expr)
+    def led(self, left, token):
+        cable = self.led_map.get(token.type)
+        if not cable:
+            raise Exception('Unexpected token', token)
+        return cable(self, left, token)
 
-            if self.token.type == TokenType.RPAREN:
-                break
+    def lbp(self, token):
+        return self.lbp_map.get(token.type, 1)
 
-            if self.token.type == TokenType.COMMA:
-                self.advance()
-                continue
 
-            raise Exception('Invalid token in led_arg_list()', self.token)
+def led_pass(parser, left, token):
+    expr = parser.expr(parser.lbp(token))
+    return MsgAstNode(
+        target=left,
+        name=expr,
+        args=None,
+    )
 
-        left.args = args
-        return left
+
+def led_arg_list(parser, left, token):
+    args = []
+
+    while True:
+        expr = parser.expr(parser.lbp(token))
+        args.append(expr)
+
+        if parser.token.type == TokenType.RPAREN:
+            break
+
+        if parser.token.type == TokenType.COMMA:
+            parser.advance()
+            continue
+
+        raise Exception('Invalid token in led_arg_list()', parser.token)
+
+    left.args = args
+    return left
+
+
+def led_assignment(parser, left, token):
+    expr = parser.expr(parser.lbp(token))
+    return AssignAstNode(left, expr)
+
+Parser.register_token(
+    TokenType.PASS,
+    lbp=10,
+    nud=None,
+    led=led_pass,
+)
+
+Parser.register_token(
+    TokenType.ASSIGNMENT,
+    lbp=9,
+    nud=None,
+    led=led_assignment,
+)
+
+Parser.register_token(
+    TokenType.IDENT,
+    lbp=8,
+    nud=(lambda token: IdentAstNode(token.value)),
+    led=(lambda parser, left, token: left),
+)
+
+Parser.register_token(
+    TokenType.STRING,
+    lbp=7,
+    nud=(lambda token: ConstAstNode(ConstType.STRING, token.value)),
+    led=(lambda parser, left, token: left),
+)
+
+Parser.register_token(
+    TokenType.INT,
+    lbp=7,
+    nud=(lambda token: ConstAstNode(ConstType.INT, token.value)),
+    led=(lambda parser, left, token: left),
+)
+
+Parser.register_token(
+    TokenType.LPAREN,
+    lbp=1,
+    nud=None,
+    led=led_arg_list,
+)
+
+Parser.register_token(
+    TokenType.RPAREN,
+    lbp=1,
+    nud=None,
+    led=(lambda parser, left, token: left),
+)
