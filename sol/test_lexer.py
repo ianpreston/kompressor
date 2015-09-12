77d11bccbf61631a8841af0e51c04b963b9a14e6
Ian Preston <hello@ianpreston.io>
2015-09-12 13:06:30 -0700

    Add unit tests for lexer

---
+++ sol/test_lexer.py
@@ -0,0 +1,45 @@
+from sol.lexer import Lexer, Token, TokenType
+
+
+def test_assignment():
+    source = 'x := Object.clone'
+
+    lexer = Lexer(source)
+    tokens = lexer.iter_match_tokens()
+    tokens = list(tokens)
+
+    assert tokens == [
+        Token(TokenType.IDENT, 'x'),
+        Token(TokenType.ASSIGNMENT, ':='),
+        Token(TokenType.IDENT, 'Object'),
+        Token(TokenType.PASS, '.'),
+        Token(TokenType.IDENT, 'clone'),
+    ]
+
+
+def test_string():
+    source = 'x := "Hello world"'
+
+    lexer = Lexer(source)
+    tokens = lexer.iter_match_tokens()
+    tokens = list(tokens)
+
+    assert tokens == [
+        Token(TokenType.IDENT, 'x'),
+        Token(TokenType.ASSIGNMENT, ':='),
+        Token(TokenType.STRING, 'Hello world'),
+    ]
+
+
+def test_multiple_idents():
+    source = 'a bar quux'
+
+    lexer = Lexer(source)
+    tokens = lexer.iter_match_tokens()
+    tokens = list(tokens)
+
+    assert tokens == [
+        Token(TokenType.IDENT, 'a'),
+        Token(TokenType.IDENT, 'bar'),
+        Token(TokenType.IDENT, 'quux'),
+    ]
